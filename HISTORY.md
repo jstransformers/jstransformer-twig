dd8255d2a58d4ff52163f5a74454026f4dcbc8dd
Rob Loach <robloach@gmail.com>
2015-08-29 11:07:53 -0400

    Add  support

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v0.1.0: 2015-08-29
+
+- Add `options.filename` support
+
 ## v0.0.1: 2015-07-22
 
 - Initial release
--- index.js
+++ index.js
@@ -6,8 +6,16 @@ exports.name = 'twig'
 exports.outputFormat = 'html'
 
 exports.compile = function (str, options) {
+  // Construct the Twig options.
   options = options || {}
   options.data = str
+  if ('filename' in options && !('path' in options)) {
+    options.path = options.filename
+  }
+
+  // Build the template.
   var template = twig(options)
+
+  // Use .bind() so that the template is "this" when rendering.
   return template.render.bind(template)
 }

ce061de008733cee4056df07225a81a017cfed4c
Rob Loach <robloach@gmail.com>
2017-05-05 14:20:55 -0400

    Add initial Functions support

--- index.js
+++ index.js
@@ -27,51 +27,64 @@ exports.compile = function (str, options) {
     }
   }
 
-  // Filters
-  // Allow options.filters to be a require() string.
-  if (typeof options.filters === 'string') {
-    try {
-      // eslint-disable-next-line import/no-dynamic-require
-      options.filters = require(options.filters)
-    } catch (err) {
-      // Nothing.
-    }
+  // Extend Filters and Functions
+  var extendable = {
+    filters: 'extendFilter',
+    functions: 'extendFunction'
   }
-  // Loop through all the given filters.
-  for (var name in options.filters || {}) {
-    if ({}.hasOwnProperty.call(options.filters, name)) {
-      switch (typeof options.filters[name]) {
-        case 'string':
-          try {
-            // Load the filter module.
-            // eslint-disable-next-line import/no-dynamic-require
-            var out = require(options.filters[name])
+  // eslint-disable-next-line guard-for-in
+  for (var extendableName in extendable) {
+    var extendFunctionName = extendable[extendableName]
+    // Allow options.filters to be a require() string.
+    if (typeof options[extendableName] === 'string') {
+      try {
+        // eslint-disable-next-line import/no-dynamic-require
+        options[extendableName] = require(options[extendableName])
+      } catch (err) {
+        // Nothing.
+      }
+    }
+    // Loop through all the given filters.
+    for (var name in options[extendableName] || {}) {
+      if ({}.hasOwnProperty.call(options[extendableName], name)) {
+        switch (typeof options[extendableName][name]) {
+          case 'string':
+            try {
+              // Load the filter module.
+              // eslint-disable-next-line import/no-dynamic-require
+              var out = require(options[extendableName][name])
 
-            // Check if the module is just a function.
-            if (typeof out === 'function') {
-              Twig.extendFilter(name, out)
-            } else if (out && (typeof out === 'object')) {
-              // Perhaps it is an associative array of functions?
-              for (var outName in out) {
-                if (typeof out[outName] === 'function') {
-                  Twig.extendFilter(outName, out[outName])
+              // Check if the module is just a function.
+              if (typeof out === 'function') {
+                Twig[extendFunctionName](name, out)
+              } else if (out && (typeof out === 'object')) {
+                // Perhaps it is an associative array of functions?
+                for (var outName in out) {
+                  if (typeof out[outName] === 'function') {
+                    Twig[extendFunctionName](outName, out[outName])
+                  }
                 }
               }
+            } catch (err) {
+              // Nothing.
             }
-          } catch (err) {
-            // Nothing.
-          }
-          break
-        case 'function':
-        default:
-          Twig.extendFilter(name, options.filters[name])
-          break
+            break
+          case 'function':
+          default:
+            Twig[extendFunctionName](name, options[extendableName][name])
+            break
+        }
       }
     }
   }
 
   // Build the template.
+  try{
   var template = twigRender(options)
+}catch(e) {
+  console.log(options)
+  console.error(e)
+}
 
   // Use .bind() so that the template is "this" when rendering.
   return template.render.bind(template)
